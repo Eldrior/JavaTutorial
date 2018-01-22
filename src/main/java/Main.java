4465caa6d4fa4fac74966488f349ff2d00cce0c8
Stephan stephan@vongreiffenstern.de <eldrior@users.noreply.github.com>
2018-01-22 21:30:19 +0100

    Einfaches Hello World! Beispiel.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+    //region variables
+    //endregion
+
+    //region methods
+    public static void main (String[] args)
+    {
+        System.out.println("Hallo Welt!");
+        System.out.println("Mein Name ist Stephan.");
+    }
+    //endregion
+
+    //region constructors
+    //endregion
+}
